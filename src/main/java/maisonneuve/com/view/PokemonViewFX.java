dc89549e459405d764ee37bfe6dc894339667397
Clement Laflamme <clement_laflamme@videotron.ca>
2026-07-09 20:41:52 -0400

    feat: rearrenger composants et debut du css

--- src/main/java/maisonneuve/com/view/PokemonViewFX.java
+++ src/main/java/maisonneuve/com/view/PokemonViewFX.java
@@ -1,6 +1,8 @@
 package maisonneuve.com.view;
 
 import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
@@ -45,6 +47,8 @@ public class PokemonViewFX {
     public Button btnCapturer;
     public Label titreListe;
     public ListView<Pokemon> listePokemonsCaptures;
+    public ImageView imageViewAsh;
+    public Separator ligneSeparation;
 
     // demande de confirmation lors de la libération d'un Pokémon
     public boolean afficherConfirmation(String titre, String message) {
@@ -73,34 +77,44 @@ public class PokemonViewFX {
 
         // Zone du haut : En-tête et barre de recherche
         titre = new Label("P O K É D E X");
-        titre.getStyleClass().add("nom");
-
-        // test CSS
-        //titre.getStyleClass().add("test");
-
+        titre.getStyleClass().add("titre");
 
         sousTitre = new Label("Recherchez tous vos pokémons préférés !");
-        sousTitre.getStyleClass().add("subtitle");
+        sousTitre.getStyleClass().add("sous-titre");
+
         barreRecherche = new TextField();
-        barreRecherche.getStyleClass().add("borderRad");
+        barreRecherche.getStyleClass().add("barre-recherche");
 
         msgErreur = new Label();
-        msgErreur.getStyleClass().add("subtitle");
+        msgErreur.getStyleClass().add("");
         VBox haut = new VBox(8, titre, sousTitre, barreRecherche, msgErreur);
+        haut.setAlignment(Pos.TOP_CENTER);
+        haut.getStyleClass().add("boite-haut");
 
         // Zone centrale : carte du pokémon
         image = new ImageView();
-        image.setFitWidth(200);
-        image.setFitHeight(200);
+        image.setFitWidth(300);
+        image.setFitHeight(300);
+        image.getStyleClass().add("image-pokemon");
 
         nomPokemon = new Label("Ex : Pikachu");
         idPokemon = new Label("Ex : #25");
-        VBox carte = new VBox(8, image, nomPokemon, idPokemon);
+        HBox nomId = new HBox(2, nomPokemon, idPokemon);
+        nomId.setAlignment(Pos.CENTER);
+
+        VBox carte = new VBox(8, image, nomId);
+        carte.setAlignment(Pos.TOP_CENTER);
+
 
         // Types (1 ou 2)
         type1 = new Label("Électrique");
         type2 = new Label("Vol");
         HBox types = new HBox(2, type1, type2);
+        types.setAlignment(Pos.CENTER);
+
+        // Ligne de séparation
+        ligneSeparation = new Separator();
+        ligneSeparation.getStyleClass().add("ligne-separation");
 
         // Poids et taille
         poidsPokemon = new Label("Ex : 24 kg");
@@ -113,6 +127,7 @@ public class PokemonViewFX {
         VBox colPoids = new VBox(3, poids, poidsPokemon);
         VBox colTaille = new VBox(3, taille, taillePokemon);
         HBox poidsTaille = new HBox(2, colPoids, colTaille);
+        poidsTaille.setAlignment(Pos.CENTER);
 
         // Statistiques
         statistiques = new Label("Statistiques de base");
@@ -145,17 +160,28 @@ public class PokemonViewFX {
         VBox colStats = new VBox(4, statPv, statAtk, statDef, statSpd);
         HBox stats = new HBox(8, colLabels, colProgressBars, colStats);
 
+        // Vbox pour la carte
+        VBox carteStats = new VBox(10, carte, types, ligneSeparation, statistiques,poidsTaille, stats);
+        carteStats.getStyleClass().add("carte-pokemon");
+        carteStats.setAlignment(Pos.CENTER);
+        VBox.setMargin(carteStats, new Insets(20));
+
         // Bouton capturer
         btnCapturer = new Button("Capturer");
+
+        //Image de Ash
+        imageViewAsh = new ImageView();
 
         // Liste des pokémons capturés
         titreListe = new Label("Liste des Pokémons capturés");
         listePokemonsCaptures = new ListView<>();
 
+        // Hbox Image Ash et liste pokémons
+        HBox imgListe = new HBox(4, imageViewAsh, listePokemonsCaptures);
+
         // Assemblage
-        VBox centre = new VBox(20, carte, types, poidsTaille, stats, btnCapturer, listePokemonsCaptures);
-
-
+        VBox centre = new VBox(2, carteStats, btnCapturer, imgListe);
+        centre.getStyleClass().add("boite-centre");
 
         // Zone du bas
         messageStatut = new Label("Ex: Pikachu chargé avec succès !");
@@ -164,6 +190,7 @@ public class PokemonViewFX {
         racine.setTop(haut);
         racine.setCenter(centre);
         racine.setBottom(messageStatut);
+        barreRecherche.maxWidthProperty().bind(racine.widthProperty().multiply(0.50));
     }
 
     public Parent getRoot() {return racine;}
